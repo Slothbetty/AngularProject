22f5807f3f986b76b7d8bcd2f0961fef67d809c9
Bei Zhao <slothbettyz@gmail.com>
2020-02-14 16:11:40 -0600

    T19 Using a service

---
+++ Codevolution/src/app/employee.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor() { }
+
+  getEmployees(){
+    return[
+    {"id": 1, "name": "Andrew", "age": 30},
+    {"id": 2, "name": "Brandon", "age": 25},
+    {"id": 3, "name": "Christina", "age": 26},
+    {"id": 4, "name": "Elena", "age": 28}
+    ]
+  }
+}
